85feb7fa543a3212faa90b25c04f1a2b47d2d35e
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-05-31 13:33:28 +0200

    Homework 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,17 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            return state.sort((a,b) => action.payload === 'up' ? (a.name > b.name ? 1 : -1) : (a.name < b.name ? 1 : -1)) // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age >= 18);
         }
-        default:
+        default: {
             return state
+        }
+
     }
 }
