06ecab82543bbc6c25923e4d49157714614fc504
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-05-31 13:45:56 +0200

    mutablility state

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,7 +7,7 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            return state.sort((a,b) => action.payload === 'up' ? (a.name > b.name ? 1 : -1) : (a.name < b.name ? 1 : -1)) // need to fix
+            return [...state].sort((a,b) => action.payload === 'up' ? (a.name > b.name ? 1 : -1) : (a.name < b.name ? 1 : -1)) // need to fix
         }
         case 'check': {
             return state.filter(u => u.age >= 18);
