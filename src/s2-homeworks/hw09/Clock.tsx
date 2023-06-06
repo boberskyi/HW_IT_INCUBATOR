1a97f02d3d6ee3d0c8b94e59341f18cd4a1a1e83
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-06-06 09:42:03 +0200

    get month

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -31,10 +31,8 @@ console.log('stop');
     const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
-    // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    // const stringDay = 'date->day' || <br/> // пишут студенты
-    const stringDay = new Date().toLocaleString('en-US', { weekday: 'long' }) || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
+    const stringDay = new Date().toLocaleString('en-US', { weekday: 'long' }) || <br/>;
+    const stringMonth = new Date().toLocaleString('en-US', { month: 'long' }) || <br/>
 
     return (
         <div className={s.clock}>
