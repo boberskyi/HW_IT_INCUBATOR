e96f25225b10ea24a032a307689b4283b3fe7cb8
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-06-06 10:16:05 +0200

    style timer

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -7,7 +7,7 @@ function Clock() {
     const [timerId, setTimerId] = useState<number | undefined>(undefined)
     // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
     const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
-    const [show, setShow] = useState<boolean>(false)
+    const [show, setShow] = useState<boolean>(true)
 
     const start = () => {
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
@@ -29,7 +29,7 @@ console.log('stop');
     }
 
     const stringTime = new Date().toLocaleTimeString([], { hour12:false, hour: '2-digit', minute: '2-digit', second: '2-digit' })|| <br/> ;// часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = new Date().toLocaleDateString('en-US', { day: '2-digit', month: '2-digit', year: 'numeric' }).replace(/\//g, '.') || <br/>; // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     const stringDay = new Date().toLocaleString('en-US', { weekday: 'long' }) || <br/>;
     const stringMonth = new Date().toLocaleString('en-US', { month: 'long' }) || <br/>
@@ -52,8 +52,8 @@ console.log('stop');
                 <div className={s.more}>
                     {show ? (
                         <>
-                            <span id={'hw9-month'}>{stringMonth}</span>,{' '}
-                            <span id={'hw9-date'}>{stringDate}</span>
+                            <span id={'hw9-date'}>{stringDate}</span>,{' '}
+                            <span id={'hw9-month'}>{stringMonth}</span>
                         </>
                     ) : (
                         <>
